0d9ede6aaa473a90b1e2f111e533e11d8a1c62f6
Bart van den Eijnden <bartvde@boundlessgeo.com>
2014-11-24 19:26:48 +0100

    Add vector layer option to example

--- examples/example.js
+++ examples/example.js
@@ -77,21 +77,45 @@
                     );
                     layer.setExtent(extent);
                     // @todo use urls for subdomain loading
-                    layer.setSource(new ol.source.TileWMS({
-                        url: url,
-                        params: {
-                            'TIME': isoDate(layer._times.start || layer._times[0]),
-                            'LAYERS': name,
-                            'VERSION': '1.1.0',
-                            'TILED': true
-                        },
-                        serverType: 'geoserver'
-                    }));
+                    if (layer instanceof ol.layer.Tile) {
+                        layer.setSource(new ol.source.TileWMS({
+                            url: url,
+                            params: {
+                                'TIME': isoDate(layer._times.start || layer._times[0]),
+                                'LAYERS': name,
+                                'VERSION': '1.1.0',
+                                'TILED': true
+                            },
+                            serverType: 'geoserver'
+                        }));
+                    } else if (layer instanceof ol.layer.Vector) {
+                        layer.setSource(new ol.source.ServerVector({
+                            format: new ol.format.GeoJSON(),
+                            loader: function(bbox, resolution, projection) {
+                                // TODO https://github.com/openlayers/ol3/issues/2972
+                                if (ol.extent.intersects(extent, bbox)) {
+                                    var wfsUrl = url;
+                                    wfsUrl += '?service=WFS&version=1.1.0&request=GetFeature&typename=' +
+                                        name + '&outputFormat=application/json&' +
+                                        '&srsName=' + projection.getCode() + '&bbox=' + bbox.join(',') + ',' + projection.getCode();
+                                    $.ajax({
+                                        url: wfsUrl
+                                    }).done(function(response) {
+                                        layer.getSource().addFeatures(layer.getSource().readFeatures(response));
+                                    });
+                                }
+                            },
+                            strategy: ol.loadingstrategy.createTile(new ol.tilegrid.XYZ({
+                                maxZoom: 19
+                            })),
+                            projection: map.getView().getProjection()
+                        }));
+                    }
                 }
             });
         }
 
-        function addLayer(name) {
+        function addLayer(name, asVector) {
             var workspace = 'geonode';
             var parts = name.split(':');
             if (parts.length > 1) {
@@ -99,7 +123,12 @@
                 name = parts[1];
             }
             var url = '/geoserver/' + workspace + '/' + name + '/wms';
-            var layer = new ol.layer.Tile({name: name});
+            var layer;
+            if (asVector === true) {
+                layer = new ol.layer.Vector({name: name});
+            } else {
+                layer = new ol.layer.Tile({name: name});
+            }
             return loadCapabilities(layer, url).then(function() {
                 map.addLayer(layer);
                 map.getView().fitExtent(layer.getExtent(), map.getSize());
@@ -140,7 +169,7 @@
         });
         $scope.addLayer = function () {
             $scope.loading = true;
-            timeControlsService.addLayer($scope.layerName).then(function () {
+            timeControlsService.addLayer($scope.layerName, $scope.asVector).then(function () {
                 if ($scope.timeControls === null) {
                     $scope.timeControls = timeControlsService.createControls();
                 }
