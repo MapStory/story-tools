d50bb0e6be2c04f8ebf8f5202623b70f2267c152
Bart van den Eijnden <bartvde@boundlessgeo.com>
2014-12-11 18:28:39 +0100

    Add support for TextSymbolizer in SLD generation

--- test/test-SLDStyleConverter.js
+++ test/test-SLDStyleConverter.js
@@ -11,8 +11,9 @@ describe('SLDStyleConverter', function() {
         });
     });
 
-    it('should convert simple stypes', inject(function(SLDStyleConverter) {
+    it('should convert simple types', inject(function(SLDStyleConverter) {
         var styleConfig = {
+            "typeName": "simple",
             "symbol": {
                 "size": 10,
                 "shape": "circle",
@@ -26,10 +27,43 @@ describe('SLDStyleConverter', function() {
                 "strokeWidth": 3,
                 "strokeStyle": "solid",
                 "strokeOpacity": 90
-            }
+            },
+            "geomType": "point"
         };
         var style = SLDStyleConverter.generateStyle(styleConfig);
-        expect(style).toBe('<p0:StyledLayerDescriptor xmlns:p0="http://www.opengis.net/sld" version="1.0.0"><p0:NamedLayer><p0:UserStyle><p0:FeatureTypeStyle><p0:Rule><p0:PointSymbolizer><p0:Graphic><p0:Mark><p0:WellKnownName>circle</p0:WellKnownName><p0:Fill><p0:CssParameter name="fill">#ff0000</p0:CssParameter><p0:CssParameter name="fill-opacity">0.8</p0:CssParameter></p0:Fill><p0:Stroke><p0:CssParameter name="stroke">#ffff00</p0:CssParameter><p0:CssParameter name="stroke-width">3</p0:CssParameter><p0:CssParameter name="stroke-opacity">0.9</p0:CssParameter></p0:Stroke></p0:Mark></p0:Graphic></p0:PointSymbolizer></p0:Rule></p0:FeatureTypeStyle></p0:UserStyle></p0:NamedLayer></p0:StyledLayerDescriptor>');
+        expect(style).toBe('<p0:StyledLayerDescriptor xmlns:p0="http://www.opengis.net/sld" version="1.0.0"><p0:NamedLayer><p0:Name>simple</p0:Name><p0:UserStyle><p0:FeatureTypeStyle><p0:Rule><p0:PointSymbolizer><p0:Graphic><p0:Mark><p0:WellKnownName>circle</p0:WellKnownName><p0:Fill><p0:CssParameter name="fill">#ff0000</p0:CssParameter><p0:CssParameter name="fill-opacity">0.8</p0:CssParameter></p0:Fill><p0:Stroke><p0:CssParameter name="stroke">#ffff00</p0:CssParameter><p0:CssParameter name="stroke-width">3</p0:CssParameter><p0:CssParameter name="stroke-opacity">0.9</p0:CssParameter></p0:Stroke></p0:Mark></p0:Graphic></p0:PointSymbolizer></p0:Rule></p0:FeatureTypeStyle></p0:UserStyle></p0:NamedLayer></p0:StyledLayerDescriptor>');
+    }));
+
+    it('should convert labels', inject(function(SLDStyleConverter) {
+        var styleConfig = {
+            "typeName": "simple",
+            "symbol": {
+                "size": 10,
+                "shape": "circle",
+                "graphic": null,
+                "graphicType": null,
+                "fillColor": "#ff0000",
+                "fillOpacity": 80
+            },
+            "stroke": {
+                "strokeColor": "#ffff00",
+                "strokeWidth": 3,
+                "strokeStyle": "solid",
+                "strokeOpacity": 90
+            },
+            "label": {
+                "attribute": "foo",
+                "fillColor": "#000000",
+                "fontFamily": "Serif",
+                "fontSize": 10,
+                "fontStyle": "normal",
+                "fontWeight": "normal"
+            },
+            "geomType": "point"
+        };
+        var style = SLDStyleConverter.generateStyle(styleConfig);
+        // TODO adapt the result for PropertyName when https://github.com/highsource/ogc-schemas/pull/32 gets resolved
+        expect(style).toBe('<p0:StyledLayerDescriptor xmlns:p0="http://www.opengis.net/sld" version="1.0.0"><p0:NamedLayer><p0:Name>simple</p0:Name><p0:UserStyle><p0:FeatureTypeStyle><p0:Rule><p0:PointSymbolizer><p0:Graphic><p0:Mark><p0:WellKnownName>circle</p0:WellKnownName><p0:Fill><p0:CssParameter name="fill">#ff0000</p0:CssParameter><p0:CssParameter name="fill-opacity">0.8</p0:CssParameter></p0:Fill><p0:Stroke><p0:CssParameter name="stroke">#ffff00</p0:CssParameter><p0:CssParameter name="stroke-width">3</p0:CssParameter><p0:CssParameter name="stroke-opacity">0.9</p0:CssParameter></p0:Stroke></p0:Mark></p0:Graphic></p0:PointSymbolizer><p0:TextSymbolizer><p0:Label><p1:PropertyName xmlns:p1="http://www.opengis.net/ogc"/></p0:Label><p0:Font><p0:CssParameter name="font-family">Serif</p0:CssParameter><p0:CssParameter name="font-size">10</p0:CssParameter><p0:CssParameter name="font-style">normal</p0:CssParameter><p0:CssParameter name="font-weight">normal</p0:CssParameter></p0:Font><p0:Fill><p0:CssParameter name="fill">#000000</p0:CssParameter></p0:Fill></p0:TextSymbolizer></p0:Rule></p0:FeatureTypeStyle></p0:UserStyle></p0:NamedLayer></p0:StyledLayerDescriptor>');
     }));
 
 });
