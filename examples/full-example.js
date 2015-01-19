a071b662adde4853635f22a0364a0837ec8b7b81
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-01-19 17:11:49 +0100

    Some fixes to support style updates again in full example, also add the case of SLD lines classify

--- examples/full-example.js
+++ examples/full-example.js
@@ -12,9 +12,28 @@
         'ui.bootstrap'
     ]);
 
-    module.controller('exampleController', function($scope, mapFactory, stTimeControlsFactory) {
+    module.controller('exampleController', function($scope, $http, ol3StyleConverter, mapFactory, stTimeControlsFactory) {
         $scope.map = mapFactory.create();
         $scope.timeControls = null;
+
+        $scope.styleChanged = function(layer) {
+            var sld = new storytools.edit.SLDStyleConverter.SLDStyleConverter();
+            if (layer instanceof ol.layer.Vector) {
+                layer.setStyle(function(feature, resolution) {
+                    return ol3StyleConverter.generateStyle(layer.style, feature, resolution);
+                });
+            } else {
+                var xml = sld.generateStyle(layer.style, layer.getSource().getParams().LAYERS, true);
+                $http({
+                    url: '/gslocal/rest/styles/' + layer.get('layerInfo').styleName + '.xml',
+                    method: "PUT",
+                    data: xml,
+                    headers: {'Content-Type': 'application/vnd.ogc.sld+xml; charset=UTF-8'}
+                }).then(function(result) {
+                    layer.getSource().updateParams({"_olSalt": Math.random()});
+                });
+            }
+        };
 
         // we currently need to lazily create the timeControls, should probably extract this
         // @todo! we don't want to create the timeControls here if there is a saved configuration
