8a6b4ca2649d943ac10b159e7dd0003943526dfc
Ian Schneider <planablediglet@gmail.com>
2015-02-15 14:12:23 -0700

    add tile loading progress bar

--- examples/common.js
+++ examples/common.js
@@ -482,4 +482,25 @@
         };
     });
 
+    module.controller('tileProgressController', function($scope) {
+        $scope.tilesToLoad = 0;
+        $scope.tilesLoadedProgress = 0;
+        $scope.$on('tilesLoaded', function(evt, remaining) {
+            $scope.$apply(function () {
+                if (remaining <= 0) {
+                    $scope.tilesToLoad = 0;
+                    $scope.tilesLoaded = 0;
+                    $scope.tilesLoadedProgress = 0;
+                } else {
+                    if (remaining < $scope.tilesToLoad) {
+                        $scope.tilesLoaded = $scope.tilesToLoad - remaining;
+                        $scope.tilesLoadedProgress = Math.floor(100 * ($scope.tilesLoaded/($scope.tilesToLoad - 1)));
+                    } else {
+                        $scope.tilesToLoad = remaining;
+                    }
+                }
+            });
+        });
+    });
+
 })();
