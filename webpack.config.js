bbc6864102bd9dc55afe7c5bcc14b957da759c98
Darin Acosta <darinjacosta@gmail.com>
2018-08-28 15:32:21 -0500

    Add webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require("webpack");
+const Path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MergeIntoSingleFilePlugin = require('webpack-merge-and-include-globally');
+
+const serverHost = "https://docker";
+
+module.exports = {
+  context: __dirname,
+  devtool: "cheap-module-source-map",
+  resolve: {
+    modules: ["node_modules"],
+    alias: {
+      jquery: Path.join(__dirname, "node_modules/jquery/dist/jquery")
+    }
+  },
+  entry: './examples/viewer.js',
+  output: {
+    filename: '[name]',
+    path: Path.resolve(__dirname, 'testdist'),
+  },
+  plugins: [
+    new MergeIntoSingleFilePlugin({
+      files: {
+       "bundle.js": [
+        // bundle vendor core
+        Path.resolve(__dirname, "examples/ol-debug.js"),
+        Path.resolve(__dirname, "node_modules/angular/angular.min.js"),
+        Path.resolve(__dirname, "node_modules/bootstrap/dist/js/bootstrap.min.js"),
+        Path.resolve(__dirname, "node_modules/nouislider/distribute/nouislider.min.js"),
+        Path.resolve(__dirname, "node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js"),
+        Path.resolve(__dirname, "node_modules/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.js")
+      ]
+    }
+    })
+  ]
+};
